5d0fd4ec2478089480e56f4175968b100d526155
Achyut Gupta <achyut10gupta@gmail.com>
2026-02-03 19:58:41 +0530

    UI flash flaw optimization + feed slice

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,22 @@
-import {Outlet, useNavigate} from "react-router-dom";
-import Navbar from './Navbar'
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { addUser } from "../utils/userSlice";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const userData = useSelector((store) => store.user);
-  
+  const [loading, setLoading] = useState(true);
 
   const fetchUser = async () => {
-    if (userData) return;
+    if (userData) {
+      setLoading(false);
+      return;
+    }
     try {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
@@ -22,8 +25,10 @@ const Body = () => {
     } catch (err) {
       if (err.response?.status === 401) {
         navigate("/login");
-      }        
+      }
       console.error(err.message);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -31,13 +36,14 @@ const Body = () => {
     fetchUser();
   }, []);
 
+  if (loading) return <div>Loading authentication...</div>;
 
   return (
-    <div>      
-        <Navbar />
-        <Outlet />        
+    <div>
+      <Navbar />
+      <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
